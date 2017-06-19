dea21839b35ccdd49d86c2287095fef150cb7bbe
Vineet Jacob Kuruvilla <vineet.jk@gmail.com>
2017-06-20 00:46:23 +0800

    udacity isolation game-playing agent project submitted version

--- agent_test.py
+++ agent_test.py
@@ -3,23 +3,70 @@ tests to run and debug your minimax and alphabeta agents locally.  The test
 cases used by the project assistant are not public.
 """
 
-import unittest
-
-import isolation
-import game_agent
+from game_agent import (AlphaBetaPlayer, MinimaxPlayer, custom_score, custom_score_3, custom_score_2)
+from sample_players import (improved_score, center_score)
 
 from importlib import reload
 
 
-class IsolationTest(unittest.TestCase):
-    """Unit tests for isolation agents"""
+#class IsolationTest(unittest.TestCase):
+#    """Unit tests for isolation agents"""
+#
+#    def setUp(self):
+#        reload(game_agent)
+#        self.player1 = sample_players.GreedyPlayer
+#        self.player2 = game_agent.AlphaBetaPlayer
+#        self.game = isolation.Board(self.player1, self.player2)
+#        game_agent.IsolationPlayer.__init__(3,game_agent.custom_score,10);
 
-    def setUp(self):
-        reload(game_agent)
-        self.player1 = "Player1"
-        self.player2 = "Player2"
-        self.game = isolation.Board(self.player1, self.player2)
 
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    from isolation import Board
+    
+#    reload(game_agent)
+    player1_win = 0;
+    player2_win = 0;
+    # create an isolation board (by default 7x7)
+    while(player1_win+player2_win)<1:
+        player2 = AlphaBetaPlayer(score_fn = improved_score);
+        player1 = MinimaxPlayer(score_fn = improved_score);
+        game = Board(player1, player2);
+    
+        # place player 1 on the board at row 3, column 3, then place player 2 on
+        # the board at row 0, column 5; display the resulting board state.  Note
+        # that the .apply_move() method changes the calling object in-place.
+        game.apply_move((3, 3))
+#        game.apply_move((2, 3))
+    #    print(game.to_string())
+    
+        # players take turns moving on the board, so player1 should be next to move
+    #    assert(player1 == game.active_player)
+    
+        # get a list of the legal moves available to the active player
+    #    print(game.get_legal_moves())
+    #
+    #    # get a successor of the current state by making a copy of the board and
+    #    # applying a move. Notice that this does NOT change the calling object
+    #    # (unlike .apply_move()).
+    #    new_game = game.forecast_move((1, 1))
+    #    assert(new_game.to_string() != game.to_string())
+    #    print("\nOld state:\n{}".format(game.to_string()))
+    #    print("\nNew state:\n{}".format(new_game.to_string()))
+    
+        # play the remainder of the game automatically -- outcome can be "illegal
+        # move", "timeout", or "forfeit"
+        winner, history, outcome = game.play()
+        
+        print("\nWinner: {}\nOutcome: {}".format(winner, outcome))
+        print(game.to_string())
+        print("Move history:\n{!s}".format(history))
+        if winner == player1:
+            print('Player 1 is the winner')
+            player1_win += 1;
+        else:
+            print('Player 2 is the winner')
+            player2_win += 1;
+    print('player1 win % = ')
+    print(player1_win/(player1_win+player2_win))
+    print('player2 win % = ')
+    print(player2_win/(player1_win+player2_win))
--- game_agent.py
+++ game_agent.py
@@ -3,6 +3,7 @@ test your agent's strength against a set of known agents using tournament.py
 and include the results in your report.
 """
 import random
+import math
 
 
 class SearchTimeout(Exception):
@@ -34,6 +35,26 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+    if game.is_winner(player) or game.is_loser(player):
+        return game.utility(player)
+    
+    #Position of the centre of the board
+    board_centre = (round(0.5*game.height)-1, round(0.5*game.width)-1); 
+    
+    #find the current position of the player
+    curr_pos = game.get_player_location(player);
+    
+    #find the position of the opponent
+    opp_pos = game.get_player_location(game.get_opponent(player));
+    
+    #Compute square of distance to board centre for both player and opponent
+    dist2c_own = (curr_pos[0]-board_centre[0])**2 + (curr_pos[1]-board_centre[1])**2;
+    dist2c_opp = (opp_pos[0]-board_centre[0])**2 + (opp_pos[1]-board_centre[1])**2;
+    
+    #return difference between the distance to board centre between opponent
+    # and player
+    return float(dist2c_opp - dist2c_own)
+    
     # TODO: finish this function!
     raise NotImplementedError
 
@@ -60,8 +81,24 @@ def custom_score_2(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+    
+    if game.is_winner(player) or game.is_loser(player):
+        return game.utility(player)
+    
+    #Get legal moves of the player and also of the opponent
+    own_moves = game.get_legal_moves(player);
+    opp_moves = game.get_legal_moves(game.get_opponent(player));
+    
+    #if opponent has no moves then return +inf
+    if not opp_moves:
+        return float("inf")
+    #return the ratio between number of moves for player and number of moves 
+    # for opponent
+    return float(len(own_moves)/len(opp_moves))
+    
     # TODO: finish this function!
     raise NotImplementedError
+    
 
 
 def custom_score_3(game, player):
@@ -86,6 +123,32 @@ def custom_score_3(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+    if game.is_winner(player) or game.is_loser(player):
+        return game.utility(player)
+    
+    #weightage for number of legal moves
+    w = 0.5;
+    
+    #get the possible legal moves
+    own_moves = game.get_legal_moves(player);
+    
+    #find the current position of the player
+    curr_pos = game.get_player_location(player);
+    
+    #compute the distance to the centre of the board from the current position
+    #closer to the centre, better is the position of the player
+    
+    board_centre = (round(0.5*game.height)-1, round(0.5*game.width)-1);
+    dist2c = (board_centre[0]-curr_pos[0])**2+(board_centre[1]-curr_pos[1])**2
+    
+    #return score for the current position with weightage w given to the number
+    #legal moves available and (1-w) weightage given to the dist2c. If distance
+    #to centre is 0, then return max score +inf
+    if dist2c == 0:
+        return float("inf")
+    else:
+        return w*len(own_moves)+(1-w)*(1/dist2c);  
+    
     # TODO: finish this function!
     raise NotImplementedError
 
@@ -112,7 +175,7 @@ class IsolationPlayer:
         positive value large enough to allow the function to return before the
         timer expires.
     """
-    def __init__(self, search_depth=3, score_fn=custom_score, timeout=10.):
+    def __init__(self, search_depth=3, score_fn=custom_score_3, timeout=10.):
         self.search_depth = search_depth
         self.score = score_fn
         self.time_left = None
@@ -170,7 +233,7 @@ class MinimaxPlayer(IsolationPlayer):
         # Return the best move from the last completed search iteration
         return best_move
 
-    def minimax(self, game, depth):
+    def minimax(self, game, depth, max_layer=True, flag_entry = False, depth_max = 0):
         """Implement depth-limited minimax search algorithm as described in
         the lectures.
 
@@ -211,7 +274,65 @@ class MinimaxPlayer(IsolationPlayer):
         """
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
-
+        
+        #Get avaiable legal moves
+        legal_moves = game.get_legal_moves();
+        if not legal_moves: #check if there are no legal moves
+            #if not topmost max layer return score else return move (-1,-1)
+            if depth != depth_max: 
+                return float("-inf")
+            else:
+                return (-1,-1)
+        
+        #Store the max depth to identify whether we are at topmost max layer
+        #Initialise best move with a random choice from available moves
+        if not flag_entry:
+            depth_max = depth;
+            best_move = random.choice(legal_moves);
+            flag_entry = True;
+        else:
+            best_move = (-1,-1)  
+        
+        #If at the deepest layer
+        if depth == 1:
+            if max_layer:
+                best_score = float("-inf");
+                for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    score_game = self.score(new_game_state, self);
+                    if score_game > best_score:
+                        best_score, best_move = score_game, move;
+            else:
+                best_score = float("inf");
+                for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    score_game = self.score(new_game_state, self);
+                    if score_game < best_score:
+                        best_score, best_move = score_game, move;
+        #If at other layers
+        else:    
+            if max_layer:
+                best_score = float("-inf");
+                for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    score_game = self.minimax(new_game_state, depth-1, False, flag_entry, depth_max);
+                    if score_game > best_score:
+                        best_score, best_move = score_game, move;
+            else:
+                best_score = float("inf");
+                for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    score_game = self.minimax(new_game_state, depth-1, True, flag_entry, depth_max);
+                    if score_game < best_score:
+                        best_score, best_move = score_game, move;
+        
+        #if at the topmost max layer return best_move else return best score
+        if depth == depth_max:
+            return best_move
+        else:
+#            print (best_score)
+            return best_score
+                
         # TODO: finish this function!
         raise NotImplementedError
 
@@ -253,11 +374,48 @@ class AlphaBetaPlayer(IsolationPlayer):
             (-1, -1) if there are no available legal moves.
         """
         self.time_left = time_left
-
+        
+        #All possible legal moves
+        legal_moves = game.get_legal_moves();
+        
+        #flag to indicate whether it's player's first move
+        first_move_flag = not game.get_player_location(game.active_player);        
+        
+        if not legal_moves:
+            #Return (-1,-1) if no legal moves available
+            return (-1,-1)
+        else:
+            if first_move_flag:
+                board_centre = (round(0.5*game.height)-1, round(0.5*game.width)-1);
+                if game.move_is_legal(board_centre):
+                    #return the centre of the board as next move if available
+                    return board_centre
+            else:
+                best_move = random.choice(legal_moves);               
+        
+        #Initialise depth as 1 
+        depth = 1;
+        
+        try:
+            #Iterative deepening(ID) search
+            while(1):
+                move = self.alphabeta(game, depth)
+                if not move or move == (-1,-1):
+                    #Stop search if endgame
+                    break
+                else:
+                    best_move = move;
+                #Increment the depth for next round of ID
+                depth += 1;
+            return best_move                
+        except SearchTimeout:            
+            return best_move
+            
         # TODO: finish this function!
         raise NotImplementedError
 
-    def alphabeta(self, game, depth, alpha=float("-inf"), beta=float("inf")):
+    def alphabeta(self, game, depth, alpha=float("-inf"), beta=float("inf"),\
+                  max_layer = True, flag_entry = False, depth_max = 0):
         """Implement depth-limited minimax search with alpha-beta pruning as
         described in the lectures.
 
@@ -284,6 +442,16 @@ class AlphaBetaPlayer(IsolationPlayer):
 
         beta : float
             Beta limits the upper bound of search on maximizing layers
+        
+        max_layer : bool
+            Flag to indicate max player or not
+        
+        flag_entry : bool
+            Flag to indicate whether this is call from get_move or part or call
+            via recursion
+            
+        depth_max : int
+            maximum search depth. Used to check when to return best_move
 
         Returns
         -------
@@ -304,6 +472,96 @@ class AlphaBetaPlayer(IsolationPlayer):
         """
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
+        
+        #Get all available legal moves
+        legal_moves = game.get_legal_moves();
+        
+        #Store the depth if it's the first call from get_move. Also initialise
+        # best_move with random choice from legal_moves. For all other recursive
+        #call to function initialise best_move with None
+        if not flag_entry:
+            depth_max = depth;
+            best_move = random.choice(legal_moves);
+            flag_entry = True;
+        else:
+            best_move = None
+        
+        #Return (-1,-1) if no legal moves available if at the topmost max layer
+        #else return score (alpha - beta)
+        if not legal_moves:
+            if depth != depth_max:
+                if max_layer:
+                    return float("-inf"), float("-inf")
+                else:
+                    return float("inf"), float("inf")
+            else:
+                return (-1,-1)
+                      
+        # Alpha-beta pruning- alternate between min layer and max layer till 
+        # depth = 1
+        #For deepest layer, depth = 1
+        if depth == 1:
+            if max_layer:
+               for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    alpha_sc, beta_sc = float("-inf"), self.score(new_game_state, self);
+                    if alpha_sc >= beta:
+                        alpha = alpha_sc;
+                        break
+                    elif beta_sc > alpha:
+                        alpha = beta_sc;
+                        best_move = move;
+                        if alpha >= beta:
+                            break
+            else:
+                for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    alpha_sc, beta_sc = self.score(new_game_state, self), float("inf");
+                    if alpha_sc <= alpha:
+                        beta = alpha_sc;
+                        break
+                    elif alpha_sc < beta:
+                        beta = alpha_sc;
+                        best_move = move;
+                        if beta <= alpha:
+                            break
+        else:        
+            # For all other layers
+            if max_layer:
+                for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    alpha_sc, beta_sc = self.alphabeta(new_game_state, depth-1,\
+                                                       alpha,beta,False, \
+                                                       flag_entry, depth_max);
+                    if alpha_sc >= beta:
+                        alpha = alpha_sc;
+                        break
+                    elif beta_sc > alpha:
+                        alpha = beta_sc;
+                        best_move = move;
+                        if alpha >= beta:
+                            break
+            else:
+                for move in legal_moves:
+                    new_game_state = game.forecast_move(move);
+                    alpha_sc, beta_sc = self.alphabeta(new_game_state, depth-1,\
+                                                 alpha,beta,True, flag_entry,\
+                                                 depth_max);
+                    if alpha_sc <= alpha:
+                        beta = alpha_sc;
+                        break
+                    elif alpha_sc < beta:
+                        beta = alpha_sc;
+                        best_move = move;
+                        if beta <= alpha:
+                            break
+        
+        #Return best_move if in first layer (max, where depth = depth_max)
+        #else return alpha, beta values
+        if depth == depth_max:
+            return best_move
+        else:
+            return alpha, beta                
 
         # TODO: finish this function!
         raise NotImplementedError
